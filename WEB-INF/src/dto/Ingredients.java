fd8f66941135c7ca145f6904214753e33dde435a
Alexandre Martel <alexandre.martel.etu@univ-lille.fr>
2024-02-19 10:26:30 +0100

    Préparation environnement

---
+++ WEB-INF/src/dto/Ingredients.java
@@ -0,0 +1,30 @@
+package dto;
+
+/**
+ * Ingredients
+ */
+public class Ingredients {
+
+    int id, prix;
+    String name;
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getPrix() {
+        return prix;
+    }
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
