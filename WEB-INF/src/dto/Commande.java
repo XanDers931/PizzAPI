a42a12d9eb725340824ee190a607140bc8718cdd
Alexandre Martel <alexandre.martel.etu@univ-lille.fr>
2024-03-08 09:17:19 +0100

    commande dto

--- WEB-INF/src/dto/Commande.java
+++ WEB-INF/src/dto/Commande.java
@@ -5,9 +5,41 @@ import java.util.ArrayList;
 
 public class Commande {
     private int id;
+    private String name;
     private Date date;
-    private String 
     private ArrayList<Pizza> commandes;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public ArrayList<Pizza> getCommandes() {
+        return commandes;
+    }
+    public void setCommandes(ArrayList<Pizza> commandes) {
+        this.commandes = commandes;
+    }
 
+    public Commande createCommande(int id, String name, Date date, ArrayList<Pizza> commandes){
+        Commande commande = new Commande();
+        commande.setId(id);
+        commande.setName(name);
+        commande.setDate(date);
+        commande.setCommandes(commandes);
+        return commande;
+    }
 
 }
