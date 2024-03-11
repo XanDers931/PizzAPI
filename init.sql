e88b8aa88e2d93974faf53a6f80dbabe5b1357a5
paul.milleville.etu <paul.milleville.etu@univ-lille.fr>
2024-03-11 16:39:15 +0100

    Ajout de commande dans le fichier sql

--- init.sql
+++ init.sql
@@ -1,6 +1,8 @@
 DROP TABLE pizza CASCADE;
 DROP TABLE ingredients CASCADE;
 DROP TABLE pizza_ingredient CASCADE;
+DROP TABLE commande CASCADE;
+DROP TABLE commande_pizza CASCADE;
 
 CREATE TABLE pizza (
   id INT NOT NULL,
@@ -23,6 +25,21 @@ CREATE TABLE pizza_ingredient (
   PRIMARY KEY (pizza_id, ingredient_id),
   FOREIGN KEY (pizza_id) REFERENCES pizza(id) ON DELETE CASCADE,
   FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+);
+
+CREATE TABLE commande (
+  id INT NOT NULL,
+  nom text NOT NULL,
+  dates date NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE commande_pizza (
+  pizza_id INT NOT NULL,
+  commande_id INT NOT NULL,
+  PRIMARY KEY (pizza_id, commande_id),
+  FOREIGN KEY (pizza_id) REFERENCES pizza(id) ON DELETE CASCADE,
+  FOREIGN KEY (commande_id) REFERENCES commande(id) ON DELETE CASCADE
 );
 
 INSERT INTO ingredients (id, name, prix) VALUES
@@ -71,3 +88,29 @@ INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES (4, 6); -- Poivron
 INSERT INTO pizza (id, nom, pate, prixBase) VALUES (5, 'Poire et Gorgonzola', 'Fine', 12);
 INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES (5, 15); -- Poire
 INSERT INTO pizza_ingredient (pizza_id, ingredient_id) VALUES (5, 14); -- Gorgonzola
+
+--Insert commande
+INSERT INTO commande (id, nom, dates) VALUES (1, 'Jean Dupont', '2024-03-10');
+INSERT INTO commande (id, nom, dates) VALUES (2, 'Marie Dubois', '2024-03-09');
+INSERT INTO commande (id, nom, dates) VALUES (3, 'Paul Martin', '2024-03-08');
+INSERT INTO commande (id, nom, dates) VALUES (4, 'Alice Lefèvre', '2024-03-07');
+INSERT INTO commande (id, nom, dates) VALUES (5, 'Thomas Richard', '2024-03-06');
+
+--Commande 1
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (2, 1);  -- Pizza Pepperoni
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (4, 1);  -- Pizza Regina
+
+--Commande 2
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (1, 2);  -- Pizza Margherita
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (3, 2);  -- Pizza Quatre Fromages
+
+--Commande 3
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (5, 3);  -- Pizza Calzone
+
+--Commande 4
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (1, 4);  -- Pizza Margherita
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (4, 4);  -- Pizza Regina
+
+--Commande 5
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (3, 5);  -- Pizza Quatre Fromages
+INSERT INTO commande_pizza (pizza_id, commande_id) VALUES (2, 5);  -- Pizza Pepperoni
